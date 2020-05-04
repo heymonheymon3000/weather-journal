363d370edb37beb773a6b493a3d3e600aa3aaa7a
Terry Parrish <tparri2@emory.edu>
2020-05-04 17:31:48 -0400

    added dynamic weather data

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
-// Require Express to run server and routes
+// Require dependencies
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+// listening callback
+function listening(){
+    console.log("server running"); 
+    console.log(`running on localhost: ${port}`);
+}
+
+// GET route returns projectData
+app.get('/all', function (request, response) {
+    response.send(projectData);
+});
+
+// POST route adds data to projectData
+app.post('/add', function (request, response) {
+    newEntry = {
+        temperature: request.body.temperature,
+        date: request.body.date,
+        userResponse: request.body.userResponse
+    };
+
+    projectData.unshift(newEntry);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,84 @@
 /* Global Variables */
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=3214852bcf51b63cd1a44bc8d045383c';
+const units = '&units=imperial';
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const init = () =>{
+    const generateButton = document.getElementById('generate');
+    generateButton.addEventListener('click', fetchWeatherData);
+}
+
+const fetchWeatherData = () =>{
+    const zip = document.getElementById('zip').value;
+    const url = baseUrl+zip+apiKey+units;
+
+    getWeatherDataByZip(url)
+    .then(function(data) {
+        let d = new Date(data.dt * 1000);
+        let date_str = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
+        const userResponse = document.getElementById('feelings').value;
+
+        postWeatherData('http://localhost:8000/add', {
+            temperature: data.main.temp, 
+            date: date_str, 
+            userResponse: userResponse
+        });
+    })
+    .then(function(){
+        return getAllWeatherData('http://localhost:8000/all');
+    })
+    .then(function(data){
+        updateUI(data);
+    });
+}
+
+const getWeatherDataByZip = async (url= '') => {
+    const response = await fetch(url);
+
+    try {
+        const weatherData = await response.json();
+        return weatherData;
+    } catch(error) {
+        console.log("error", error);
+    }
+}
+
+const postWeatherData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type':'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch(error) {
+        console.log('error', error);
+    };
+};
+
+const getAllWeatherData = async (url = '') => {
+    const response = await fetch(url);
+
+    try {
+        const weatherData = await response.json();
+        return weatherData;
+    } catch(error) {
+        console.log('error', error);
+    };
+};
+
+const updateUI = (data) => {
+    if(data.length > 0) {
+        document.getElementById('date').innerHTML = data[0].date;
+        document.getElementById('temp').innerHTML = data[0].temperature;
+        document.getElementById('content').innerHTML = data[0].userResponse;
+    }
+};
+
+window.addEventListener("DOMContentLoaded", init);
